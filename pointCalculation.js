da2a3457266c82bd5efd20dbd42212290ae89234
haemie <jmezhang@gmail.com>
2023-10-29 18:38:01 -0500

    created file for holding point calculation methods

---
+++ pointCalculation.js
@@ -0,0 +1,67 @@
+module.exports = {
+  // One point for every alphanumeric character in the retailer name.
+  retailerName: (receipt) => {
+    const { retailer } = receipt;
+    const alphanumericRetailer = retailer
+      .split('')
+      .filter((e) => /[a-zA-Z0-9]/.test(e));
+    return alphanumericRetailer.length;
+  },
+  // 50 points if the total is a round dollar amount with no cents.
+  roundTotal: (receipt) => {
+    const { total } = receipt;
+    if (Number(total) % 1 === 0) {
+      return 50;
+    } else {
+      return 0;
+    }
+  },
+  // 25 points if the total is a multiple of 0.25.
+  roundQuarterTotal: (receipt) => {
+    const { total } = receipt;
+    if (Number(total) % 0.25 === 0) {
+      return 25;
+    } else {
+      return 0;
+    }
+  },
+  // 5 points for every two items on the receipt.
+  itemsCount: (receipt) => {
+    const { items } = receipt;
+    return Math.floor(items.length / 2) * 5;
+  },
+  // If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+  itemDescriptionCheck: (receipt) => {
+    const { items } = receipt;
+    let points = 0;
+    items.forEach((e) => {
+      if (e.shortDescription && e.shortDescription.length % 3 === 0) {
+        // if there is no shortDescription, default to 0
+        points += Math.ceil(Number(e.price || 0) * 0.2); // if there is no price, default to 0
+      }
+    });
+    return points;
+  },
+  // 6 points if the day in the purchase date is odd.
+  oddDate: (receipt) => {
+    const { purchaseDate } = receipt;
+    const date = new Date(purchaseDate);
+    const day = date.getDate();
+    if (day % 2 === 1) {
+      return 6;
+    } else {
+      return 0;
+    }
+  },
+  // 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+  twoToFour: (receipt) => {
+    const { purchaseDate, purchaseTime } = receipt;
+    const date = new Date(`${purchaseDate} ${purchaseTime}`);
+    const hour = date.getHours();
+    if (hour > 14 && hour < 16) {
+      return 10;
+    } else {
+      return 0;
+    }
+  },
+};
