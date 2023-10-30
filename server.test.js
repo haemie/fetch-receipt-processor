94514613ec8b7e5e4d201c5a91f0ca6e90ad8e18
haemie <jmezhang@gmail.com>
2023-10-29 19:28:45 -0500

    added test cases for 2 given example receipts

--- server.test.js
+++ server.test.js
@@ -1,3 +1,5 @@
+const pointCalculation = require('./pointCalculation');
+
 const exampleReceipt1 = {
   retailer: 'Target',
   purchaseDate: '2022-01-01',
@@ -56,26 +58,88 @@ const exampleReceipt2 = {
 
 const exampleResult2 = 109;
 
-describe('Passes example receipts', async () => {
-  it('should fulfill example receipt 1', async () => {
-    const response = await fetch('/receipts/process', {
-      method: 'post',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({ receipt: exampleReceipt1 }),
-    });
-    expect(response).toBe(exampleResult1);
+describe('point calculations for example receipt 1', () => {
+  test('points for 6 long retailer name', () => {
+    expect(pointCalculation.retailerName(exampleReceipt1.retailer)).toBe(6);
   });
-
-  it('should fulfill example receipt 2', async () => {
-    const response = await fetch('/receipts/process', {
-      method: 'post',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({ receipt: exampleReceipt2 }),
-    });
-    expect(response).toBe(exampleResult2);
+  test('points for non round number total', () => {
+    expect(pointCalculation.roundTotal(exampleReceipt1.total)).toBe(0);
+  });
+  test('points for total not payable using quarters', () => {
+    expect(pointCalculation.roundQuarterTotal(exampleReceipt1.total)).toBe(0);
+  });
+  test('points for 5 items', () => {
+    expect(pointCalculation.itemsCount(exampleReceipt1.items)).toBe(10);
+  });
+  test('points for item descriptions', () => {
+    expect(pointCalculation.itemDescriptionCheck(exampleReceipt1.items)).toBe(
+      6
+    );
+  });
+  test('points for odd purchase date', () => {
+    expect(pointCalculation.oddDay(exampleReceipt1.purchaseDate)).toBe(6);
+  });
+  test('points for non 2-4pm', () => {
+    expect(
+      pointCalculation.twoToFour(
+        exampleReceipt1.purchaseDate,
+        exampleReceipt1.purchaseTime
+      )
+    ).toBe(0);
   });
 });
+
+describe('point calculations for example receipt 2', () => {
+  test('points for retailer name with non alphanumerics', () => {
+    expect(pointCalculation.retailerName(exampleReceipt2.retailer)).toBe(14);
+  });
+  test('points for round number total', () => {
+    expect(pointCalculation.roundTotal(exampleReceipt2.total)).toBe(50);
+  });
+  test('points for total payable using quarters', () => {
+    expect(pointCalculation.roundQuarterTotal(exampleReceipt2.total)).toBe(25);
+  });
+  test('points for 4 items', () => {
+    expect(pointCalculation.itemsCount(exampleReceipt2.items)).toBe(10);
+  });
+  test('points for item descriptions', () => {
+    expect(pointCalculation.itemDescriptionCheck(exampleReceipt2.items)).toBe(
+      0
+    );
+  });
+  test('points for even purchase date', () => {
+    expect(pointCalculation.oddDay(exampleReceipt2.purchaseDate)).toBe(0);
+  });
+  test('points for 2-4pm', () => {
+    expect(
+      pointCalculation.twoToFour(
+        exampleReceipt2.purchaseDate,
+        exampleReceipt2.purchaseTime
+      )
+    ).toBe(10);
+  });
+});
+
+// describe('Passes example receipts', async () => {
+//   it('should fulfill example receipt 1', async () => {
+//     const response = await fetch('/receipts/process', {
+//       method: 'post',
+//       headers: {
+//         'Content-type': 'application/json',
+//       },
+//       body: JSON.stringify({ exampleReceipt1 }),
+//     });
+//     expect(response).toBe(exampleResult1);
+//   });
+
+//   it('should fulfill example receipt 2', async () => {
+//     const response = await fetch('/receipts/process', {
+//       method: 'post',
+//       headers: {
+//         'Content-type': 'application/json',
+//       },
+//       body: JSON.stringify({ exampleReceipt2 }),
+//     });
+//     expect(response).toBe(exampleResult2);
+//   });
+// });
